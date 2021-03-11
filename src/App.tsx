18076166ce95d7a6049be35e61b9ea05b27c6f74
julien92 <julien.cornille92@gmail.com>
2021-03-11 20:23:24 +0100

    Add settings url

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,45 @@ import React from 'react';
 
 import './App.css';
 import useDataObserver from './hooks/useWar3Observer';
-import { Route, HashRouter } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  useLocation,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import Settings from './Settings';
 import Overlay from './Overlay';
 
 function App() {
-  const { data } = useDataObserver();
-
-  if (!data || data.type !== 'state') return null;
-
   return (
-    <HashRouter>
-      <Route path="/" exact>
-        <Overlay state={data} />
-      </Route>
-      <Route path="/settings" exact>
-        <Settings></Settings>
-      </Route>
-    </HashRouter>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route path="/?settings=true" exact></Route>
+        <Redirect />
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
+// A custom hook that builds on useLocation to parse
+// the query string for you.
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+const Redirect = () => {
+  const { data } = useDataObserver();
+  let query = useQuery();
+  const isSettings = query.get('settings') ? true : false;
+  if (!isSettings) {
+    if (!data || data.type !== 'state') return null;
+  }
+
+  return (
+    <div className="overlay">
+      {query.get('settings') ? <Settings></Settings> : <Overlay state={data} />}
+    </div>
+  );
+};
