2fa5a035f90c6b65d06cd3fb8dfd11d7b46a9820
pedrus <pierre.nole@gmail.com>
2021-06-19 13:09:22 +0200

    feat: 2v2 gold and XP graphs

--- src/Overlay/components/ExperienceGraphModal/useExperienceDifferenceData.ts
+++ src/Overlay/components/ExperienceGraphModal/useExperienceDifferenceData.ts
@@ -2,7 +2,15 @@ import { useContext, useEffect, useRef } from 'react';
 
 import { GameStateContext } from '../../../contexts';
 import useDataHistory from '../../../hooks/useDataHistory';
+import Hero from '../../../models/Hero';
+import Player from '../../../models/Player';
 import State from '../../../models/State';
+
+const concatTeamHeroes = (players: Player[], teamIndex: number) => {
+  return players
+    .filter((player) => player.team_index === teamIndex)
+    .reduce((heroes, player) => heroes.concat(player.heroes), [] as Hero[]);
+};
 
 const useExperienceDifferenceData = (state: State) => {
   const { gameSpeed } = useContext(GameStateContext);
@@ -15,16 +23,18 @@ const useExperienceDifferenceData = (state: State) => {
   useEffect(() => {
     if (gameSpeed === 0) return;
 
-    const p1TotalExperience = state.content.players[0].heroes.reduce(
+    const team1Heroes = concatTeamHeroes(state.content.players, 0);
+    const team1TotalExperience = team1Heroes.reduce(
       (sum, { experience }) => sum + experience,
       0
     );
-    const p2TotalExperience = state.content.players[1].heroes.reduce(
+    const team2Heroes = concatTeamHeroes(state.content.players, 1);
+    const team2TotalExperience = team2Heroes.reduce(
       (sum, { experience }) => sum + experience,
       0
     );
     addData({
-      value: p1TotalExperience - p2TotalExperience,
+      value: team1TotalExperience - team2TotalExperience,
       gameTime: state.content.game.game_time,
     });
   }, [addData, gameSpeed, state]);
